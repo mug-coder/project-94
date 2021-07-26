a69eb36ca0e51d82c2a25265a4fbd87fec94107f
mug-coder <81393910+mug-coder@users.noreply.github.com>
2021-07-26 11:07:00 -0400

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBAcIg5n0_kwr-38yirxNoHmzoRiP64XC0",
+    authDomain: "kwitter-app-project-faa6e.firebaseapp.com",
+    projectId: "kwitter-app-project-faa6e",
+    storageBucket: "kwitter-app-project-faa6e.appspot.com",
+    messagingSenderId: "312932985781",
+    appId: "1:312932985781:web:af0ca287dc1768d0f3c7fc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
